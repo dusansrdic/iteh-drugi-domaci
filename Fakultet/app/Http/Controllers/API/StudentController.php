ac0ca4394ad4b0934888cdaa1e5d9b6cfc09cca4
dusansrdic <dusansrdic00@gmail.com>
2022-12-11 16:54:00 +0100

    PUT api/student/{student} + DELETE api/student/{student}

--- Fakultet/app/Http/Controllers/API/StudentController.php
+++ Fakultet/app/Http/Controllers/API/StudentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Resources\StudentResource;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -74,10 +75,38 @@ class StudentController extends Controller
      * @param  \App\Models\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Student $student)
+    public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'ime' => 'required|string',
+            'prezime' => 'required|string',
+            'email' => 'required|email',
+            'telefon' => 'required|string',
+		    'budzet' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->ime = $request->ime;
+            $student->prezime = $request->prezime;
+            $student->email = $request->email;
+            $student->telefon= $request->telefon;
+            $student->budzet = $request->budzet;
+
+
+            $student->save();
+
+            return response()->json(['Uspesno su sacuvane izmene studenta!',new StudentResource($student)]);
+        } else {
+            return response()->json('Ne moze se pronaci student sa zahtevanim id-em!');
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
@@ -85,8 +114,15 @@ class StudentController extends Controller
      * @param  \App\Models\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Student $student)
+    public function destroy($id)
     {
-        //
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json(['Uspesno brisanje!', new StudentResource($student)]);
+        } else {
+            return response()->json('Neuspesno brisanje, ID nije pronadjen');
+        }
     }
+
 }
